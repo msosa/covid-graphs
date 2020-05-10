38abbdac4d1bc34a4de9576215a1ac614fd4c4c7
msosa <m.sosa1127@gmail.com>
2020-05-10 14:08:36 -0400

    Get graphs to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'covid-graphs';
+export class AppComponent implements OnInit {
+	constructor(private httpClient: HttpClient) {
+	}
+	title = 'covid-graphs';
+	selectedStates = ['New York'];
+	statesData: any[][];
+	states: string[];
+	dataHolders: CovidGraphHolder[];
+
+	ngOnInit(): void {
+		this.stateChanged();
+		this.httpClient.get('/api/covid/names').subscribe(s => this.states = s as string[]);
+	}
+
+	stateChanged() {
+		this.httpClient.get('/api/covid?names=' + this.selectedStates.join(',')).subscribe(
+			data => {
+				this.statesData = data as any[];
+				this.generateData();
+			}
+		);
+	}
+
+	generateData() {
+		const dataHolders: CovidGraphHolder[] = [];
+		graphs.forEach(graph => {
+			const holder = new CovidGraphHolder();
+			dataHolders.push(holder);
+			holder.name = graph;
+			this.statesData.forEach((states) => {
+				const multi = new GraphObject();
+				holder.multi.push(multi);
+				let skipChange = graph.endsWith('Change');
+				let skipAvg = graph.endsWith('Avg');
+				states.forEach((day, index) => {
+					multi.name = day.state;
+					if (!skipChange && !skipAvg) {
+						multi.series.push({name: day.date, value: day[graph]});
+					}
+					skipChange = false;
+					if (index > 3) {
+						skipAvg = false;
+					}
+				});
+			});
+		});
+		this.dataHolders = dataHolders;
+	}
 }
+
+class CovidGraphHolder {
+	name: string;
+	multi: GraphObject[] = [];
+}
+
+class GraphObject {
+	name: string;
+	series: GraphValue[] = [];
+}
+
+class GraphValue {
+	name: string;
+	value: any;
+}
+
+const graphs = [
+	'confirmed',
+	'confirmedChange', 'confirmedRollingAvg', 'confirmedPercentage',
+	'deaths', 'deathsChange', 'deathsRollingAvg',
+	'tested', 'testedChange', 'testedRollingAvg'
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		NgxChartsModule,
+		MatSelectModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		ReactiveFormsModule,
+		FormsModule,
+		DragDropModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
